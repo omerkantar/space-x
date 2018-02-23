c38e9fda5a56acabae6ec880537103786b5f2785
omer <iomerkantar@gmail.com>
2018-02-23 21:54:23 +0300

    init: proje dogdu 👶🏿

---
+++ spacex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  spacex
+//
+//  Created by omer kantar on 23.02.2018.
+//  Copyright © 2018 arabam.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
